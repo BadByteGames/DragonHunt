c0802a519c693ed0d07aa9924979f7042f9d19ae
armedturret <rjwhitenh@outlook.com>
2018-03-23 19:51:05 -0400

    Added triggers

--- DragonHunt/DragonHunt/Player.cpp
+++ DragonHunt/DragonHunt/Player.cpp
@@ -1,6 +1,6 @@
 #include "Player.h"
 
-
+#include "Logger.h"
 
 Player::Player()
 {
@@ -9,4 +9,41 @@ Player::Player()
 
 Player::~Player()
 {
+}
+
+void Player::triggerEvent(std::string name)
+{
+	//checks if the trigger is local or not
+	if (name.length > 0 && name[0] == '!') {
+		//destination trigger is local
+	}
+	else {
+		//create a global trigger event in the m_triggers if not already existing
+		auto it = m_triggers.find(name);
+		if (it != m_triggers.end()) {
+			//already exists
+			it->second = true;
+			Logger::logEvent("player", name + " triggered");
+		}
+		else {
+			m_triggers.insert(std::make_pair(name, true));
+			Logger::logEvent("player", name + " triggered");
+		}
+	}
+}
+
+bool Player::wasTriggered(std::string name)
+{
+	//checks if global trigger event has been triggered and if not, creates a new trigger
+	auto it = m_triggers.find(name);
+	if (it != m_triggers.end()) {
+		//already exists, just return value
+		return it->second;
+	}
+	else {
+		//create a new trigger of that name
+		m_triggers.insert(std::make_pair(name, false));
+
+		return false;
+	}
 }
--- DragonHunt/DragonHunt/Player.h
+++ DragonHunt/DragonHunt/Player.h
@@ -1,8 +1,23 @@
 #pragma once
+
+#include <string>
+#include <unordered_map>
+
 class Player
 {
 public:
 	Player();
 	~Player();
+
+	//the player class will be managing triggers and event calling
+	void triggerEvent(std::string name);
+
+	//mostly called by events to check if a certain thing was triggered
+	bool wasTriggered(std::string name);
+
+private:
+	//map of all the GLOBAL triggers (not prefixed by !)
+	//name, wasTriggered
+	std::unordered_map<std::string, bool> m_triggers;
 };
 
