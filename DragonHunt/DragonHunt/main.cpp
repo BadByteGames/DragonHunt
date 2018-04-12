21c61a7b948dc286ff1bf3407376a49677f93c94
armedturret <rjwhitenh@outlook.com>
2018-04-12 11:07:27 -0400

    Added https requests as well as restored cross platform stuff

--- DragonHunt/DragonHunt/main.cpp
+++ DragonHunt/DragonHunt/main.cpp
@@ -8,9 +8,12 @@
 #include <string>
 #include <iostream>
 
+#if defined(WIN32) || defined(_WIN32)
 #include <winhttp.h>
+#endif
 
 std::string getFileFromSite(std::string address) {
+#if defined(WIN32) || defined(_WIN32)
 	std::string webAddress = "";
 	std::string pathToFile = "";
 	
@@ -121,6 +124,8 @@ std::string getFileFromSite(std::string address) {
 	if (hConnect) WinHttpCloseHandle(hConnect);
 	if (hSession) WinHttpCloseHandle(hSession);
 	return outval;
+#endif
+	return "This platform does not support online requests.";
 }
 
 class DefaultDragonHuntCMD : public EasyCmd::CMD {
@@ -141,6 +146,25 @@ public:
 			a.loadFromFile(getString("file"));
 			a.destroy();
 		}
+		else if (getString("https") != "") {
+			//creates a local copy of the file in the https request
+			std::string https = getString("https");
+			
+			size_t fileStart = https.find_last_of("//");
+
+			std::string fileName = "(https)"+https.substr(fileStart+1);
+
+			//now get the value of the string
+			std::string fileContents = getFileFromSite(https);
+			std::ofstream outFile(fileName);
+			outFile << fileContents;
+			outFile.close();
+
+			//now start the adventure
+			Adventure a;
+			a.loadFromFile(fileName);
+			a.destroy();
+		}
 		return 0;
 	}
 };
@@ -152,6 +176,13 @@ int main(int argc, char** argv) {
 	
 	//add some help tips
 	Logger::logEvent("help", "errors will have the [error] tag proceeding them (hit ctrl+f to find them)");
+	
+	std::string args = "";
+	for (int i = 0; i < argc; i++) {
+		args += argv[i];
+		args += " ";
+	}
+	Logger::logEvent("main", "Arguments: "+args);
 
 	//create app info for easycmd
 	EasyCmd::AppInfo inf;
